7711026bb4bd6c92d19e9c0ff3cd416899d547f2
Erik <eeckee@gmail.com>
2022-04-01 11:54:09 +0200

    swb: split into kfold

---
+++ datasets_turntaking/switchboard/kfold_dialogs.py
@@ -0,0 +1,42 @@
+from os.path import join
+from sklearn.model_selection import KFold
+
+from datasets_turntaking.utils import read_txt, write_txt
+from datasets_turntaking.switchboard.switchboard import INCLUDES_ASIDES
+
+
+KFOLD_ROOT = "datasets_turntaking/switchboard/files/kfolds"
+
+
+def write_kfolds(train_val, path=None):
+    if path is None:
+        path = KFOLD_ROOT
+    kf = KFold(n_splits=11)
+    for ii, (train_index, test_index) in enumerate(kf.split(train_val)):
+        tmp_train = []
+        for i in train_index:
+            tmp_train.append(train_val[i])
+        tmp_val = []
+        for i in test_index:
+            tmp_val.append(train_val[i])
+        print("val: ", len(tmp_val))
+        print("train: ", len(tmp_train))
+        write_txt(tmp_val, join(path, f"{ii}_fold_val.txt"))
+        write_txt(tmp_train, join(path, f"{ii}_fold_train.txt"))
+
+
+if __name__ == "__main__":
+
+    # Load all files/dialogs-numbers
+    train = read_txt("datasets_turntaking/switchboard/files/train.txt")
+    val = read_txt("datasets_turntaking/switchboard/files/val.txt")
+    test = read_txt("datasets_turntaking/switchboard/files/test.txt")
+
+    # combine train+val but omit those which INCLUDES_ASIDES
+    train_val = []
+    for n_dialog in train + val:
+        if not n_dialog in INCLUDES_ASIDES:
+            train_val.append(n_dialog)
+
+    # save kfolds
+    write_kfolds(train_val)
