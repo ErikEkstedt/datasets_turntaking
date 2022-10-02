2869a462ad42b5384385f06f504aa43bf08a1e5e
Erik <eeckee@gmail.com>
2022-10-02 18:13:07 +0200

    frontend: started audio frontend. only interesting with events + model output

--- frontend/dialog_audio_interface.py
+++ frontend/dialog_audio_interface.py
@@ -4,9 +4,12 @@ from htbuilder.units import unit
 import numpy as np
 import matplotlib.pyplot as plt
 
+import torch
 import tokenizers
 from turngpt.tokenizer import SpokenDialogTokenizer
 from datasets_turntaking.dataset.spoken_dialog import load_fisher, load_switchboard
+from datasets_turntaking.dialog_audio_dataset import DialogAudioDataset
+from datasets_turntaking.features.plot_utils import plot_vad_oh, plot_melspectrogram
 
 
 @st.cache
@@ -27,7 +30,10 @@ def load_dset(dataset, split, omit_bc, omit_ov):
             format_turns=True,
             num_proc=1,
         )
-    return dset
+
+    return DialogAudioDataset(
+        dataset=dset, audio_mono=False, type="sliding", vad_history=False, vad_hz=50
+    )
 
 
 @st.cache(
@@ -148,12 +154,25 @@ if __name__ == "__main__":
         )
 
         d = st.session_state.dset[st.session_state.sample_idx]
-        st.text(f"Session: {d['session']}   |   Turns: {len(d['dialog'])}")
+        st.text(f"Session: {d['session']}")
+        st.text(d.keys())
 
-        st.header("Voice Activity")
-        va_stats = vad_statistics(d["vad"])
-        st.pyplot(va_stats["vad_histogram"])
-
-        st.header("Dialog")
-        html_string = dialog_to_html(d["dialog"])
-        st.markdown(html_string, unsafe_allow_html=True)
+        fig, ax = plt.subplots(3, 1, figsize=(9, 6), height_ratios=[1, 1.5, 1])
+        plot_melspectrogram(d["waveform"][0, 0], ax=ax[0])
+        ax[0].set_xticks([])
+        ax[1].plot(d["waveform"][0, 0, ::4] + 1, color="b")
+        ax[1].plot(d["waveform"][0, 1, ::4] - 1, color="r")
+        ax[1].hlines(
+            y=0, xmin=0, xmax=len(d["waveform"][0, 0, ::4]), color="k", linewidth=1
+        )
+        ax[1].set_xlim((0, len(d["waveform"][0, 0, ::4])))
+        ax[1].set_ylim((-2, 2))
+        ax[1].set_xticks([])
+        ax[1].set_yticks([-1, 1])
+        ax[1].set_yticklabels(["B", "A"])
+        plot_melspectrogram(d["waveform"][0, 1], ax=ax[2])
+        plt.tight_layout()
+        plt.subplots_adjust(
+            left=None, bottom=None, right=None, top=None, wspace=None, hspace=0
+        )
+        st.pyplot(fig)
